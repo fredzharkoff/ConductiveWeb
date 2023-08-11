38147d53b4f06386e583bf91df5fd919522cf311
fredzharkoff <93277155+fredzharkoff@users.noreply.github.com>
2023-08-11 16:12:11 -0700

    fix nav

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function barMenu() {
+    var x = document.getElementById("navCont");
+    if (x.className === "container-fluid") {
+      x.className += " responsive";
+    } else {
+      x.className = "container-fluid";
+    }
+  } 
